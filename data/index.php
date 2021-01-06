4588095cad5742956c3a6ae2c732aa5a23bd0062
daanBouwman <472857@student.saxion.nl>
2021-01-06 21:47:45 +0100

    Reformated all the code

--- data/index.php
+++ data/index.php
@@ -1,20 +1,17 @@
 <?php
-     
-    $db = new SQLite3('../Weather_station.db');
-    $jsonArray = array();
-    //$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
-   $stations = $db -> query('select  * from Weather_station');
-    while ($row = $stations->fetchArray(1)){
 
-        $statement = $db->prepare('SELECT * FROM Readings WHERE node_id = :id order by time desc limit 1');
-        $statement->bindValue(':id', $row['node_id']);
-        $result = $statement->execute();
+$db = new SQLite3('../Weather_station.db');
+$jsonArray = array();
+//$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
+$stations = $db->query('select  * from Weather_station');
+while ($row = $stations->fetchArray(1)) {
 
-        $data = $result->fetchArray(1);
-        array_push($data, array('location' => $row['location']));
-        $jsonArray[] = $data;
-    
-    }
-    echo json_encode($jsonArray);
+    $statement = $db->prepare('SELECT * FROM Readings WHERE node_id = :id order by time desc limit 1');
+    $statement->bindValue(':id', $row['node_id']);
+    $result = $statement->execute();
 
-?>  
+    $data = $result->fetchArray(1);
+    array_push($data, array('location' => $row['location']));
+    $jsonArray[] = $data;
+}
+echo json_encode($jsonArray);
--- data_graph_day/index.php
+++ data_graph_day/index.php
@@ -1,23 +1,20 @@
 <?php
-     
-    $db = new SQLite3('../Weather_station.db');
-    $jsonArray = array();
-    //$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
-   $stations = $db-> query('select  * from Weather_station');
-    while ($row = $stations->fetchArray(1)){
 
-        $statement = $db->prepare("SELECT temperature, ambient_light, barometric_pressure, time FROM Readings where node_id = :id and time >= date('now', '-1 day') order by time desc");
-        $statement->bindValue(':id', $row['node_id']);
-        $station = $statement->execute();
+$db = new SQLite3('../Weather_station.db');
+$jsonArray = array();
+//$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
+$stations = $db->query('select  * from Weather_station');
+while ($row = $stations->fetchArray(1)) {
 
-        $data = array();
-        while ($result = $station->fetchArray(1)){
-            $data[] =  $result;
-        }
-        $row[] = $data;
-        $jsonArray[] = $row;
+    $statement = $db->prepare("SELECT temperature, ambient_light, barometric_pressure, time FROM Readings where node_id = :id and time >= date('now', '-1 day') order by time desc");
+    $statement->bindValue(':id', $row['node_id']);
+    $station = $statement->execute();
 
+    $data = array();
+    while ($result = $station->fetchArray(1)) {
+        $data[] =  $result;
     }
-    echo json_encode($jsonArray);
-
-?>  
+    $row[] = $data;
+    $jsonArray[] = $row;
+}
+echo json_encode($jsonArray);
