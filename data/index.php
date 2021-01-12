262e0a41052596bb4693e945f6096a8b38dc8195
daanBouwman <472857@student.saxion.nl>
2021-01-12 22:58:22 +0100

    Comment all the code

--- data/index.php
+++ data/index.php
@@ -1,17 +1,27 @@
 <?php
+//AUTHOR: Daan Bouwman
 
+//initialize database connection
 $db = new SQLite3('../Weather_station.db');
+//create empty array to store the data
 $jsonArray = array();
 //$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
+//get all the different weather stations
 $stations = $db->query('select  * from Weather_station');
 while ($row = $stations->fetchArray(1)) {
 
+    //request data from the weather stations from readinigs list
+    //this is the prepare statement and doesn't directly send the request. and :id is an variable which has te be assigned
     $statement = $db->prepare('SELECT * FROM Readings WHERE node_id = :id order by time desc limit 1');
+    //assing the :id variable to the node_id of the weather station
     $statement->bindValue(':id', $row['node_id']);
+    //execute and store the data
     $result = $statement->execute();
 
+    //push the data to the array
     $data = $result->fetchArray(1);
     array_push($data, array('location' => $row['location']));
     $jsonArray[] = $data;
 }
+//send the data to the user
 echo json_encode($jsonArray);
--- data_graph_week/index.php
+++ data_graph_week/index.php
@@ -1,20 +1,33 @@
 <?php
+//AUTHOR: Daan Bouwman
 
+//initialize the database connection
 $db = new SQLite3('../Weather_station.db');
+//create an empty array to store the data
 $jsonArray = array();
 //$stations = $db->query('select Weather_station.location, x.node_id, x.time, x.temperature, x.ambient_light, x.barometric_pressure from Readings x inner join Weather_station on x.node_id = Weather_station.node_id join (select p.node_id, max(time) as max_time from Readings p group by p.node_id) y on y.node_id = x.node_id and  y.max_time = x.time group by x.node_id, x.time');
+//get a list of weather stations
 $stations = $db->query('select  * from Weather_station');
 while ($row = $stations->fetchArray(1)) {
-
+       
+    //request the last 10 entries from the weather stations from readinigs list
+    //this is the prepare statement and doesn't directly send the request. and :id is an variable which has te be assigned
     $statement = $db->prepare("SELECT temperature, ambient_light, barometric_pressure, time FROM Readings where node_id = :id and time >= date('now', '-1 week') order by time desc");
+     //assign :id
     $statement->bindValue(':id', $row['node_id']);
+    $statement->bindValue(':id', $row['node_id']);
+    //execute the SQL query
     $station = $statement->execute();
 
+    //create another array to store the readings and store it  in an array
     $data = array();
     while ($result = $station->fetchArray(1)) {
         $data[] =  $result;
     }
+    //add all the readings to the specific weather stations
     $row[] = $data;
+    //add the weather stations with all the data to the array
     $jsonArray[] = $row;
 }
+//returns the data to the user
 echo json_encode($jsonArray);
